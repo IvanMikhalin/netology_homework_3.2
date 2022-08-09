1dfeff8d11691c8810cf07c4837a89d577bea6d3
Ivan Mikhalin <mikhalin_ivan@mail.ru>
2022-08-09 15:11:31 +0300

    Names changed

--- src/main.java
+++ src/main.java
@@ -1,19 +1,19 @@
 public class main {
     public static void main(String[] args) {
 
-        int initial_balance = 100;
-        int payment = 1891;
-        int bonus_per_payment = 100;
+        int initialBalance = 100;
+        int payment = 1000;
+        int bonusPerPayment = 100;
         int bonus;
 
         if (payment >= 1000) {
-            bonus = payment / bonus_per_payment;
+            bonus = payment / bonusPerPayment;
         } else {
             bonus = 0;
         }
-        int total = initial_balance + payment + bonus;
+        int total = initialBalance + payment + bonus;
 
-        System.out.println("Баланс Вашего лицевого счета составляет " + initial_balance + " руб.");
+        System.out.println("Баланс Вашего лицевого счета составляет " + initialBalance + " руб.");
         System.out.println("Вы пополнили баланс на " + payment + " руб.");
         System.out.println("Вам начисленно " + bonus + " боннусов и Ваш итоговый баланс составляет " + total + " руб.");
 
